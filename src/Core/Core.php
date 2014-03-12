1846144a05bec1dba2e4d76a0ca0459ef64707bd
Gian Santillan <giansantillan18@gmail.com>
2014-03-13 00:06:40 +0800

    added hook pre-loader fix

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -21,12 +21,22 @@
 			add_action('init',	array($this, 'init'));
 			return $this;
 		}
+		
 		public function init()
 		{
 			TwigCore::factorize();
 			return $this;
 		}
-
+	
+		public function launchHookLoaders()
+		{
+			$hookLoaders = include base_path()."/hooks.php";
+			foreach($hookLoaders as $class) {
+				$class::make()->load();
+			}
+			return $this;
+		}
+		
 		public static function config($config_key)
 		{
 			return self::$instance->config[$config_key];
