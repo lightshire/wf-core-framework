b7f14d830dc18dddab3785d1b8bb40ea0574d62a
Gian Santillan <giansantillan18@gmail.com>
2014-03-13 00:35:52 +0800

    added page interface

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -4,10 +4,25 @@
 	use \Core\TwigCore;
 	use \Core\DoctrineCore;
 	class Core
-	{
+	{	
+		/**
+		 * A Private instance of the object
+		 * @var Core
+		 */
 		private static $instance = null;
+
+		/**
+		 * The Configuration File (found at app.php)
+		 * @var array
+		 */
 		private $config = array();
 
+
+		/**
+		 * Instance generator of the Object
+		 * @param  array  $config the config file found at app.php
+		 * @return Core         The generated config instance
+		 */
 		public static function make($config = array())
 		{	
 			self::$instance = new Core;
@@ -16,18 +31,37 @@
 			return self::$instance;	
 		}
 
+
+		/**
+		 * The Main Hook loader (Hooks to 'init') 
+		 * @return Core the generated instance
+		 */
 		public function load()
 		{
 			add_action('init',	array($this, 'init'));
 			return $this;
 		}
 		
+		/**
+		 * Core Foundation Loader - this loads the core requirements
+		 * of the framework. 
+		 * @return Object The generated instance
+		 */
 		public function init()
 		{
 			TwigCore::factorize();
 			return $this;
 		}
 	
+		/**
+		 * loads all the hooks of all the classes required by the plugin
+		 * this hook loader only requires the class name, since static
+		 * call is going to be created, two functions are required, the
+		 * `make` and the `load`. It should be of type static and non-static
+		 * respectively.
+		 * 		
+		 * @return Core The generated instance
+		 */
 		public function launchHookLoaders()
 		{
 			$hookLoaders = include base_path()."/hooks.php";
@@ -37,6 +71,11 @@
 			return $this;
 		}
 		
+		/**
+		 * Getsthe config setting from the main config repository
+		 * @param  mixed $config_key The key to the array
+		 * @return mixed             The returned configuration setting
+		 */
 		public static function config($config_key)
 		{
 			return self::$instance->config[$config_key];
---
+++ src/Core/Page.php
@@ -0,0 +1,24 @@
+<?php
+	namespace Core;
+
+
+	/**
+	 *  An Interface Class implementation of the `Page` class.
+	 *  The Page class would be the skeleton of all classes 
+	 *  that are pages in the plugin
+	 */
+	interface Page
+	{
+		/**
+		 * Make generates an instance
+		 * @return Page generated instance
+		 */
+		public static function make();
+
+		/**
+		 * Load - loads all the hooks to the wordpress framework
+		 * @return Page generated instance
+		 */
+		public function load();
+
+	}
