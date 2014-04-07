dd949b4dbe125dcfa079c4bd8d4916f1417d8a22
Gian Santillan <giansantillan18@gmail.com>
2014-04-07 23:36:49 +0800

    added final capability for twig functions testing

--- src/Core/TwigCore.php
+++ src/Core/TwigCore.php
@@ -17,6 +17,15 @@
 
 			//loading functions
 			self::$twig->addFunction('sanitized_current_url', new \Twig_Function_Function('sanitized_current_url'));
+			self::$twig->addFunction('base_path', new \Twig_Function_Function('base_path'));
+			self::$twig->addFunction('cache_path', new \Twig_Function_Function('cache_path'));
+			self::$twig->addFunction('base_url', new \Twig_Function_Function('base_url'));
+			self::$twig->addFunction('plugin_url', new \Twig_Function_Function('plugin_url'));
+			self::$twig->addFunction('assets_url', new \Twig_Function_Function('assets_url'));
+			self::$twig->addFunction('dd', new \Twig_Function_Function('dd'));
+			self::$twig->addFunction('posts_url', new \Twig_Function_Function('posts_url'));
+			self::$twig->addFunction('current_url', new \Twig_Function_Function('current_url'));
+			
 
 			return self::$twig;
 		}
